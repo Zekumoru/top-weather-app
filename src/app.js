8ea5d01418fb2e9d141c29bb9d5f28f610631d03
Zekumoru Dragonhart <dev@zekumoru.com>
2022-11-16 17:13:28 +0100

    Create getCityCoord async function

--- src/app.js
+++ src/app.js
@@ -1,3 +1,21 @@
 import '@mdi/font/css/materialdesignicons.css';
+import axios from 'axios';
 import 'normalize.css';
 import './style.css';
+import API_KEYS from './.api-keys.json';
+
+async function getWeather(city) {
+  const { lat, lon } = await getCityCoord(city);
+}
+
+async function getCityCoord(city) {
+  const { data } = await axios.get('http://api.openweathermap.org/geo/1.0/direct', {
+    params: {
+      q: city,
+      appid: API_KEYS.openweathermap,
+    },
+  });
+  return data[0];
+}
+
+getWeather('London');

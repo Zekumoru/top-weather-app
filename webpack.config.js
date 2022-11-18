8970561a0e7ec771964a48992bc285062d099863
Zekumoru Dragonhart <dev@zekumoru.com>
2022-11-18 13:00:25 +0100

    Add /images static path

--- webpack.config.js
+++ webpack.config.js
@@ -19,9 +19,15 @@ module.exports = {
   },
   devtool: 'inline-source-map',
   devServer: {
-    static: {
-      directory: path.resolve(__dirname, 'dist'),
-    },
+    static: [
+      {
+        directory: path.resolve(__dirname, 'dist'),
+      },
+      {
+        directory: path.resolve(__dirname, 'src/images'),
+        publicPath: '/images',
+      },
+    ],
     watchFiles: [
       path.resolve(__dirname, 'src/*.html'),
     ],

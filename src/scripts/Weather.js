4832a3f2be9f30f1f215beea921ef50a69781905
Zekumoru Dragonhart <dev@zekumoru.com>
2022-11-23 11:18:51 +0100

    Add wmo icon conversion

--- src/scripts/Weather.js
+++ src/scripts/Weather.js
@@ -1,5 +1,7 @@
+import SvgLoader from '@zekumoru-dev/svg-loader/SvgLoader';
 import axios from 'axios';
 import API_KEYS from '../.api-keys.json';
+import wmoIconConversion from './wmo-icon-conversion.json';
 
 const Weather = {};
 export default Weather;
@@ -21,6 +23,24 @@ Weather.get = async function (city) {
     },
   })).data;
   return weather;
+};
+
+Weather.getIcon = async function (wmo, daytime = true) {
+  const time = (daytime) ? 'weather-day' : 'weather-night';
+  try {
+    const path = `images/${time}/${wmoIconConversion[wmo].full}.svg`;
+    return await SvgLoader.load(path, {
+      colorable: true,
+    });
+  }
+  catch (error) {
+    if (error instanceof TypeError) {
+      throw new TypeError(`Missing icon for wmo code ${wmo}`);
+    }
+    else {
+      throw new Error(`Could not load icon with wmo code ${wmo}`);
+    }
+  }
 };
 
 async function getCityCoord(city) {

7a6e2694d896f7b012f3c2e11c0e98ab59fa355b
Zekumoru Dragonhart <dev@zekumoru.com>
2022-11-24 14:24:29 +0100

    Create CardsDisplay.js

---
+++ src/scripts/CardsDisplay.js
@@ -0,0 +1,22 @@
+export default class {
+  #display;
+
+  constructor(display) {
+    this.#display = display;
+  }
+
+  append(card) {
+    const li = document.createElement('li');
+
+    li.appendChild(card.icon);
+    li.insertAdjacentHTML('beforeend', '<p class="time"></p>');
+    li.querySelector('.time').textContent = card.time;
+    li.appendChild(card.infoList);
+
+    this.#display.appendChild(li);
+  }
+
+  render(cards) {
+    cards.forEach(this.append.bind(this));
+  }
+}
